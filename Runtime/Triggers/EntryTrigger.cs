4321cb1914b7876ef020cec51e41b00063203339
Akela Morse <akela.morse@gmail.com>
2024-11-15 17:14:05 +0100

    Moved EntryTrigger and ExitTrigger to the Triggers namespace

---
+++ Runtime/Triggers/EntryTrigger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Akela.Bridges;
+using UnityEngine;
+
+namespace Akela.Triggers
+{
+    [AddComponentMenu("Signals/Entry Trigger", -2)]
+    [DisallowMultipleComponent]
+    public class EntryTrigger : MonoBehaviour, ITrigger
+    {
+        #region Component Fields
+        [SerializeField] BridgedEvent onAwake;
+        [SerializeField] BridgedEvent onEnable;
+        [SerializeField] BridgedEvent onStart;
+        #endregion
+
+        public bool IsActive { get; private set; }
+
+        public void AddListener(Action callback, TriggerEventType eventType = TriggerEventType.OnBecomeActive)
+        {
+            if (eventType != TriggerEventType.OnBecomeActive)
+                return;
+
+            onEnable.AddListener(() => callback());
+        }
+
+        #region Component Messages
+        private void Awake()
+        {
+            onAwake.Invoke();
+        }
+
+        private void OnEnable()
+        {
+            IsActive = true;
+
+            onEnable.Invoke();
+        }
+
+        private void Start()
+        {
+            onStart.Invoke();
+        }
+        #endregion
+    }
+}
---
+++ Runtime/Triggers/ExitTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Akela.Bridges;
+using UnityEngine;
+
+namespace Akela.Triggers
+{
+    [AddComponentMenu("Signals/Exit Trigger", -1)]
+    [DisallowMultipleComponent]
+    public class ExitTrigger : MonoBehaviour, ITrigger
+    {
+        #region Component Fields
+        [SerializeField] BridgedEvent onDisable;
+        [SerializeField] BridgedEvent onDestroy;
+        #endregion
+
+        public bool IsActive { get; private set; }
+
+        public void AddListener(Action callback, TriggerEventType eventType = TriggerEventType.OnBecomeActive)
+        {
+            if (eventType != TriggerEventType.OnBecomeActive)
+                return;
+
+            onDisable.AddListener(() => callback());
+        }
+
+        #region Component Messages
+        private void OnDisable()
+        {
+            IsActive = true;
+            
+            onDisable.Invoke();
+        }
+
+        private void OnDestroy()
+        {
+            onDestroy.Invoke();
+        }
+        #endregion
+    }
+}
