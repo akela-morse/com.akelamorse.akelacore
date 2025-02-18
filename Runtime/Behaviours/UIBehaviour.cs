15de313bcc55aa4df5c04b6bec4adbbe7bf24b48
Akela Morse <akela.morse@gmail.com>
2025-02-18 16:11:53 +0100

    Made UIBehaviour extend Unity's own UIBehaviour to benefit from special messages

--- Runtime/Behaviours/UIBehaviour.cs
+++ Runtime/Behaviours/UIBehaviour.cs
@@ -2,7 +2,7 @@
 
 namespace Akela.Behaviours
 {
-    public abstract class UIBehaviour : MonoBehaviour
+    public abstract class UIBehaviour : UnityEngine.EventSystems.UIBehaviour
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Field hiding")]
         public new RectTransform transform => (RectTransform)base.transform;
