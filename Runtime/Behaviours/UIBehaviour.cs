e47eaf747c4a8372463e0721672a1193a12f2fc7
Akela Morse <akela.morse@gmail.com>
2025-02-18 10:16:56 +0100

    Made UIBehaviour field hiding public

--- Runtime/Behaviours/UIBehaviour.cs
+++ Runtime/Behaviours/UIBehaviour.cs
@@ -5,6 +5,6 @@ namespace Akela.Behaviours
     public abstract class UIBehaviour : MonoBehaviour
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Field hiding")]
-        protected new RectTransform transform => (RectTransform)base.transform;
+        public new RectTransform transform => (RectTransform)base.transform;
     }
 }
