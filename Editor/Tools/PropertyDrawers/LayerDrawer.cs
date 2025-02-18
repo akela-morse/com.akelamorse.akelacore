9def9509499529cf3b2f314f643209a4d48f6ffe
Akela Morse <akela.morse@gmail.com>
2025-02-18 13:43:12 +0100

    Implemented Layer Attribute

---
+++ Editor/Tools/PropertyDrawers/LayerDrawer.cs
@@ -0,0 +1,21 @@
+﻿using Akela.Tools;
+using UnityEditor;
+using UnityEngine;
+
+namespace AkelaEditor.Tools
+{
+    [CustomPropertyDrawer(typeof(LayerAttribute))]
+    internal class LayerDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (property.propertyType != SerializedPropertyType.Integer)
+            {
+                GUI.Label(position, "Use [EulerAngles] with Int32 fields.");
+                return;
+            }
+
+            property.intValue = EditorGUI.LayerField(position, label, property.intValue);
+        }
+    }
+}
