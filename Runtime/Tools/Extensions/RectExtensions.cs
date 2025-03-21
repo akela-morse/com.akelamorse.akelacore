b70fab3f2cab42f2c1e37ab4379fb396fa4a7a38
Akela Morse <akela.morse@gmail.com>
2025-03-21 16:15:27 +0100

    Added RectExtensions

---
+++ Runtime/Tools/Extensions/RectExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Akela.Tools
+{
+    public static class RectExtensions
+    {
+        public static Rect Scale(this Rect r, float factor)
+        {
+            r.min *= factor;
+            r.max *= factor;
+
+            return r;
+        }
+    }
+}
