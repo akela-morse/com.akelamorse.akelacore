6a83c3663d2f105ea28fcfc45e8a396de3cf7cf4
Akela Morse <akela.morse@gmail.com>
2025-06-19 17:55:01 +0200

    Implemented NavMeshAgentExtensions

---
+++ Runtime/Tools/Extensions/NavMeshAgentExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Akela.Tools
+{
+    public static class NavMeshAgentExtensions
+    {
+        public static bool HasReachedDestination(this NavMeshAgent agent)
+        {
+            return !agent.pathPending && agent.remainingDistance <= agent.stoppingDistance && (!agent.hasPath || agent.desiredVelocity == Vector3.zero);
+        }
+    }
+}
