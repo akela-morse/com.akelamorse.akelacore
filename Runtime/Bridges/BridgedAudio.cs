c8123833075538b55c1a0493077bd2f27d8cefac
Akela Morse <akela.morse@gmail.com>
2025-11-18 17:01:13 +0100

    BridgedAudio now uses AudioResource instead of AudioClip

--- Runtime/Bridges/BridgedAudio.cs
+++ Runtime/Bridges/BridgedAudio.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using Fikkis.Data;
 #elif AKELA_FMOD
 using FMODUnity;
+#else
+using UnityEngine.Audio;
 #endif
 
 namespace Akela.Bridges
@@ -15,14 +17,20 @@ namespace Akela.Bridges
         [SerializeField] private AudioEvent _internalValue;
 
         public static implicit operator AudioEvent(BridgedAudio bridge) => bridge._internalValue;
+
+        public static implicit operator bool(BridgedAudio bridge) => bridge._internalValue;
 #elif AKELA_FMOD
         [SerializeField] private EventReference  _internalValue;
 
         public static implicit operator EventReference (BridgedAudio bridge) => bridge._internalValue;
-#else
-        [SerializeField] private AudioClip _internalValue;
 
-        public static implicit operator AudioClip(BridgedAudio bridge) => bridge._internalValue;
+        public static implicit operator bool(BridgedAudio bridge) => !bridge._internalValue.IsNull;
+#else
+        [SerializeField] private AudioResource _internalValue;
+
+        public static implicit operator AudioResource(BridgedAudio bridge) => bridge._internalValue;
+
+        public static implicit operator bool(BridgedAudio bridge) => bridge._internalValue;
 #endif
     }
 }
