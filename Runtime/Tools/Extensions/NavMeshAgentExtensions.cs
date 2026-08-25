c367e55d893f8e4c651b28ef9c3dd606b4ebcba7
Akela Morse <akela.morse@gmail.com>
2026-08-25 07:11:39 +0200

    Added two NavMeshAgent warp methods with path preservation

--- Runtime/Tools/Extensions/NavMeshAgentExtensions.cs
+++ Runtime/Tools/Extensions/NavMeshAgentExtensions.cs
@@ -12,12 +12,43 @@ namespace Akela.Tools
             return !agent.pathPending && agent.remainingDistance <= agent.stoppingDistance && (!agent.hasPath || agent.desiredVelocity == Vector3.zero);
         }
 
+        public static bool WarpPreservePath(this NavMeshAgent agent, Vector3 position)
+        {
+            var hasDest = !agent.HasReachedDestination();
+            var dest =  agent.destination;
+
+            if (!agent.Warp(position))
+                return false;
+
+            if (hasDest)
+                agent.SetDestination(dest);
+
+            return true;
+        }
+
         public static bool TryWarp(this NavMeshAgent agent, Vector3 position)
         {
             if (!NavMesh.SamplePosition(position, out _, 1f, new NavMeshQueryFilter { agentTypeID = agent.agentTypeID, areaMask = agent.areaMask }))
                 return false;
 
             return agent.Warp(position);
+        }
+
+        public static bool TryWarpPreservePath(this NavMeshAgent agent, Vector3 position)
+        {
+            if (!NavMesh.SamplePosition(position, out _, 1f, new NavMeshQueryFilter { agentTypeID = agent.agentTypeID, areaMask = agent.areaMask }))
+                return false;
+
+            var hasDest = !agent.HasReachedDestination();
+            var dest =  agent.destination;
+
+            if (!agent.Warp(position))
+                return false;
+
+            if (hasDest)
+                agent.SetDestination(dest);
+
+            return true;
         }
     }
 }
