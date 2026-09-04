d2c08937e06349f1e0fdcb55d4b029a6aa805502
Akela Morse <akela.morse@gmail.com>
2026-09-04 08:50:36 +0200

    Added ExpandByFactor

---
+++ Runtime/Tools/Extensions/QuaternionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace Akela.Tools
+{
+    public static class QuaternionExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Quaternion ExpandByFactor(this Quaternion quaternion, float factor)
+        {
+            quaternion.ToAngleAxis(out var angle, out var axis);
+            return Quaternion.AngleAxis(angle * factor, axis);
+        }
+    }
+}
